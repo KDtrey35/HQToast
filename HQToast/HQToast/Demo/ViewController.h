1ab3d575f5e64699fcda02fa2bf8ad1f1ada13e1
KDtrey35 <823394434@qq.com>
2018-04-23 17:04:52 +0800

    add HQToast
    add HQToast Demo

---
+++ HQToast/HQToast/Demo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  HQToast
+//
+//  Created by HQ on 2018/4/23.
+//  Copyright © 2018年 HQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (strong, nonatomic) UIButton *toastButton;
+@end
+
